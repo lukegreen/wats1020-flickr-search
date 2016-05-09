6a2914136969b21c21c4c0a18d8c8b85adbe6382
elukuent <elukuent@gmail.com>
2016-05-09 00:01:12 -0400

    basic requirements

--- js/test.js
+++ js/test.js
@@ -1,15 +1,8 @@
-
-$(document).on('ready', function(){
-    // Place your code here, inside the document ready handler.
-
-    // Create a function called `searchImages()`. This function will handle the
-    // process of taking a user's search terms and sending them to Flickr for a
-    // response.
+$(document).on('ready', function() {
+   
   var searchImages = function(tags){
-    
   //Accessing the API, iterating over JSON array, grabbing the URL for a new 'img' that is then appended
   //to the list of items.
-    
   var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
    
     $.getJSON( flickrAPI, {
@@ -23,10 +16,8 @@ $(document).on('ready', function(){
         console.log(itemSource);
         var newImage = $('<img>').attr('src', itemSource);
         var newListItem = $('<li>').append(newImage);
-        var newTitle = $('<p class="image-title">').html(item.title).appendTo(newListItem);
-        var newDate = $('<p class="image-date">').text(item.date_taken).appendTo(newListItem);
-        var newLink = $('<a>').attr('href', item.link).text('View on Flickr.').appendTo(newListItem);
         $(newListItem).appendTo('#images');
+          
         if (i === 10) {
           return false;
         }
@@ -35,16 +26,14 @@ $(document).on('ready', function(){
   };
  
  //setting up the button even handler to take input and pass it to the 'searchImages' function   
-  
     $('button.search').on('click', function(event){
       event.preventDefault();
       var searchValue = $(event.target.parentElement).find('input[name="searchText"]')[0];
       console.log(searchValue.value);
       searchImages(searchValue.value);
       
-    });       
-  
-    // STRETCH GOAL: Add a "more info" popup using the technique shown on the
-    // Bootstrap Modal documentation: http://getbootstrap.com/javascript/#modals-related-target
-
-});
+    });
+    
+    
+})
+            
